e4936755185164aa67faad405ba3367bb8ad47dc
root <v.smorodinsky@klienti.ru>
2019-12-23 22:47:28 +0300

    fix .env

--- config.php
+++ config.php
@@ -1,4 +1,6 @@
 <?php
+//print_r($_REQUEST); exit;
+
 if (!empty($_REQUEST['project'])) {
     $project=$_REQUEST['project'];
     $dotenv = \Dotenv\Dotenv::create(__DIR__, ".env.".$project);
@@ -7,17 +9,17 @@ if (!empty($_REQUEST['project'])) {
 else if (file_exists('.env')) {
     $dotenv = \Dotenv\Dotenv::create(__DIR__);
     $dotenv->load();
-
-
-
-
-    $dsn = 'mysql:host=localhost;dbname=' . $_ENV['DB_NAME'];
-    $options = array(
-        PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8mb4',
-    );
-    $db = new \PDO($dsn, $_ENV['DB_USER'], $_ENV['DB_PASS'], $options);
 }
 else{
     echo "Отсутствует файл .env";
     exit;
 }
+
+//print_r($_ENV);exit;
+
+$dsn = 'mysql:host=localhost;dbname=' . $_ENV['DB_NAME'];
+$options = array(
+    PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8mb4',
+);
+$db = new \PDO($dsn, $_ENV['DB_USER'], $_ENV['DB_PASS'], $options);
+//echo "333";exit;
--- run.php
+++ run.php
@@ -2,6 +2,5 @@
 require 'vendor/autoload.php';
 require "config.php";
 use YourResult\JiraReport\JiraReport;
-
-$z=new JiraReport();
+$z = new JiraReport($db);
 $z->run();
